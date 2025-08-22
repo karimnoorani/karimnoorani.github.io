dfacf7b192aa49be4ef4cafd0fcd722a13e5f6f6
Karim Noorani <59754833+karimnoorani@users.noreply.github.com>
2025-08-22 16:34:56 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -37,6 +37,7 @@ const CONFIG = {
           description: 'Designed and developed a React + TypeScript web application to help editorial, product, and data teams explore content performance data. Implemented features including keyword search, filters, sortable/paginated tables, and CSV export. Integrated interactive data visualization using Recharts to highlight top tags and engagement patterns. Applied custom CSS/Tailwind styling for a responsive, accessible UI with polished dashboards and tag components.',
           imageUrl: 'https://i.ibb.co/mCt4vHgr/Screenshot-2025-08-22-at-4-26-55-PM.png',
           link: 'https://contentexplorer.playcode.io'
+        },
         {
           title: 'Internal Data Platform',
           description:
